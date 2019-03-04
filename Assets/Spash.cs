bc68abab6f614ad806ef37524d6aa3c88a990ba4
cdouaihyucsd <cdouaihy@ucsd.edu>
2019-03-03 22:27:12 -0800

    World Building and Camera Movement

---
+++ Assets/Spash.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Spash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("LoadWorld", 2f);
+    }
+
+    void LoadWorld()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    void Awake () {
+        DontDestroyOnLoad(gameObject);
+	}
+}
